183ee76dc96df7180c057077d830cc483d77aad4
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2023-01-28 13:23:27 +0100

    refactor code return currently playing if available but return last played if no song is currently playing

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const client_id = process.env.client_id;
 const client_secret = process.env.client_secret;
 const token_url = process.env.token_url;
 const access_url = process.env.access_url;
+const base_url = `https://api.spotify.com/v1/me/player`;
+const accountBaseUrl = `https://accounts.spotify.com/api`;
 
 app.use(cors());
 
@@ -32,104 +34,171 @@ const generateRandomString = (length) => {
 
 const stateKey = "spotify_auth_state";
 
-app.get("/currently-playing", async (req, res) => {
-  axios({
+const getUserProfile = (access_token) => {
+  const res = axios({
+    url: `https://api.spotify.com/v1/me`,
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+      "Content-Type": "application/json",
+    },
+  });
+  return res;
+};
+
+const getAccessToken = () => {
+  const accessTokenResponse = axios({
     url: token_url,
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((tokenRes) => {
-    if (tokenRes.status === 200) {
-      axios({
-        url: access_url,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }).then((DbResponse) => {
-        if (DbResponse.status === 200) {
-          if (tokenRes.data.expiryTime > now) {
-            axios({
-              url: `https://api.spotify.com/v1/me/player/recently-played`,
-              headers: {
-                Authorization: `Bearer ${tokenRes.data.access_token}`,
-                "Content-Type": "application/json",
-              },
-            }).then((trackResponse) => {
-              if (trackResponse.status === 200) {
-                res.status(200).send(trackResponse.data.items[0].track);
-              } else {
-                res.status(400).json({
-                  message: `Error occured`,
-                });
-              }
-            });
-          }
-          if (tokenRes.data.expiryTime < now) {
-            axios({
-              method: "post",
-              url: "https://accounts.spotify.com/api/token",
-              data: querystring.stringify({
-                grant_type: "refresh_token",
-                refresh_token: DbResponse.data.refresh_token,
-              }),
-              headers: {
-                "content-type": "application/x-www-form-urlencoded",
-                Authorization: `Basic ${new Buffer.from(
-                  `${client_id}:${client_secret}`
-                ).toString("base64")}`,
-              },
-            })
-              .then((response) => {
-                if (response.status === 200) {
-                  const { access_token } = response.data;
-                  let expiredTime = currDate + 3600;
-                  axios({
-                    method: "put",
-                    url: token_url,
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    data: JSON.stringify({
-                      access_token: access_token,
-                      expiryTime: expiredTime,
-                    }),
-                  }).then((putRes) => {
-                    if (putRes.status === 200) {
-                      axios({
-                        url: `https://api.spotify.com/v1/me/player/recently-played`,
-                        headers: {
-                          Authorization: `Bearer ${access_token}`,
-                          "Content-Type": "application/json",
-                        },
-                      }).then((currListSong) => {
-                        if (currListSong.status === 200) {
-                          res
-                            .status(200)
-                            .send(currListSong.data.items[0].track);
-                        } else {
-                          res.status(400).json({
-                            message: `Error occured`,
-                          });
-                        }
-                      });
-                    }
-                  });
-                } else {
-                  console.log(response.expires_in);
-                }
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          }
-        } else {
-          console.log(DbResponse);
-        }
-      });
-    } else {
-      console.log(tokenRes);
-    }
   });
+  return accessTokenResponse;
+};
+
+const getRefreshToken = () => {
+  const refreshTokenRes = axios({
+    url: access_url,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return refreshTokenRes;
+};
+
+const fetchLastPlayedSong = (payload) => {
+  const res = axios({
+    url: `${base_url}/recently-played`,
+    headers: {
+      Authorization: `Bearer ${payload}`,
+      "Content-Type": "application/json",
+    },
+  });
+  return res;
+};
+
+const fetchCurrPlayingSong = (payload) => {
+  const res = axios({
+    url: `${base_url}/currently-playing`,
+    headers: {
+      Authorization: `Bearer ${payload}`,
+      "Content-Type": "application/json",
+    },
+  });
+  return res;
+};
+
+const getNewAccessToken = (payload) => {
+  const res = axios({
+    method: "post",
+    url: `${accountBaseUrl}/token`,
+    data: querystring.stringify({
+      grant_type: "refresh_token",
+      refresh_token: payload.refresh_token,
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${client_id}:${client_secret}`
+      ).toString("base64")}`,
+    },
+  });
+  return res;
+};
+
+const postNewAccessToken = (payload) => {
+  const res = axios({
+    method: "put",
+    url: token_url,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      access_token: payload.access_token,
+      expiryTime: payload.expiredTime,
+    }),
+  });
+  return res;
+};
+
+const loginPostAccessToken = (payload) => {
+  const res = axios({
+    method: "put",
+    url: access_url,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      access_token: payload.access_token,
+      refresh_token: payload.refresh_token,
+      expiryTime: payload.expiryTime,
+    }),
+  });
+  return res;
+};
+
+const getInitAccessToken = (payload) => {
+  const res = axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "authorization_code",
+      code: payload.code,
+      redirect_uri: payload.redirect_uri,
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${client_id}:${client_secret}`
+      ).toString("base64")}`,
+    },
+  });
+  return res;
+};
+
+app.get("/currently-playing", async (req, res) => {
+  try {
+    const accessToken = await getAccessToken();
+    const refreshToken = await getRefreshToken();
+    if (accessToken.data.expiryTime > now) {
+      const currPlayingSong = await fetchCurrPlayingSong(
+        accessToken.data.access_token
+      );
+      if (currPlayingSong.status === 204) {
+        const lastPlayedSong = await fetchLastPlayedSong(
+          accessToken.data.access_token
+        );
+        if (lastPlayedSong.status === 200) {
+          res.status(200).send(lastPlayedSong.data.items[0].track);
+        }
+      } else if (currPlayingSong.status === 200) {
+        res.status(200).send(currPlayingSong.data.item);
+      }
+    }
+    if (accessToken.data.expiryTime < now) {
+      const getNewAccess = await getNewAccessToken({
+        refresh_token: refreshToken.data.refresh_token,
+      });
+      const { access_token } = getNewAccess.data;
+      let expiredTime = currDate + 3600;
+      const postNewAccess = await postNewAccessToken({
+        access_token: access_token,
+        expiredTime: expiredTime,
+      });
+      if (postNewAccess.status === 200) {
+        const currPlayingSong = await fetchCurrPlayingSong(access_token);
+        if (currPlayingSong.status === 204) {
+          const lastPlayedSong = await fetchLastPlayedSong(access_token);
+          if (lastPlayedSong.status === 200) {
+            res.status(200).send(lastPlayedSong.data.items[0].track);
+          }
+        } else if (currPlayingSong.status === 200) {
+          res.status(200).send(currPlayingSong.data.item);
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error.response);
+  }
 });
 
 app.get("/login", (req, res) => {
@@ -147,120 +216,57 @@ app.get("/login", (req, res) => {
   res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
-app.get("/callback", (req, res) => {
-  const code = req.query.code || null;
-
-  axios({
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
-    data: querystring.stringify({
-      grant_type: "authorization_code",
+app.get("/callback", async (req, res) => {
+  try {
+    const code = req.query.code || null;
+    const initialAccessToken = await getInitAccessToken({
       code: code,
       redirect_uri: redirect_uri,
-    }),
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${new Buffer.from(
-        `${client_id}:${client_secret}`
-      ).toString("base64")}`,
-    },
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        const { access_token, refresh_token, expires_in } = response.data;
-        let time = Date.now();
-        let expiryTime = time + expires_in;
-        axios({
-          url: `https://api.spotify.com/v1/me`,
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-            "Content-Type": "application/json",
-          },
-        }).then((response) => {
-          if (response.status === 200) {
-            console.log(response.data.id);
-            if (response.data.id === "22tlod5b4sujuk2jxfja6cjjy") {
-              axios({
-                method: "put",
-                url: access_url,
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                data: JSON.stringify({
-                  access_token: access_token,
-                  refresh_token: refresh_token,
-                  expiryTime: expiryTime,
-                }),
-              }).then((res) => {
-                if (res.status === 200) {
-                } else {
-                  console.log(res.data);
-                }
-              });
-            }
-          }
-        });
-        const queryParams = querystring.stringify({
-          access_token,
-          refresh_token,
-        });
-        res.redirect(`http://localhost:3000/?${queryParams}`);
-      } else {
-        res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
-      }
-    })
-    .catch((error) => {
-      res.send(error);
     });
+    if (initialAccessToken.status === 200) {
+      const { access_token, refresh_token, expires_in } =
+        initialAccessToken.data;
+      let time = Date.now();
+      let expiryTime = time + expires_in;
+      const userProfile = await getUserProfile(access_token);
+      if (userProfile.status === 200) {
+        if (userProfile.data.id === "22tlod5b4sujuk2jxfja6cjjy") {
+          const postLoginAccessToken = await loginPostAccessToken({
+            access_token: access_token,
+            refresh_token: refresh_token,
+            expiryTime: expiryTime,
+          });
+          console.log(postLoginAccessToken);
+        }
+      }
+      const queryParams = querystring.stringify({
+        access_token,
+        refresh_token,
+      });
+      res.redirect(`http://localhost:3000/?${queryParams}`);
+    }
+  } catch (error) {
+    res.send(error.response);
+  }
 });
 
-app.get("/refresh_token", (req, res) => {
-  axios({
-    url: access_url,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((response) => {
-    if (response.status === 200) {
-      console.log(response.data);
-      axios({
-        method: "post",
-        url: "https://accounts.spotify.com/api/token",
-        data: querystring.stringify({
-          grant_type: "refresh_token",
-          refresh_token: response.data.refresh_token,
-        }),
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-          Authorization: `Basic ${new Buffer.from(
-            `${client_id}:${client_secret}`
-          ).toString("base64")}`,
-        },
-      })
-        .then((response) => {
-          let timer = Date.now();
-          let expiryTime = timer + response.data.expires_in;
-          axios({
-            method: "put",
-            url: token_url,
-            headers: {
-              "Content-Type": "application/json",
-            },
-            data: JSON.stringify({
-              access_token: response.data.access_token,
-              expiryTime: expiryTime,
-            }),
-          });
-          console.log(response.data);
-          res.send(response.data);
-        })
-        .catch((error) => {
-          res.send(error);
-        });
-    } else {
-      console.log(response.data);
-    }
-  });
+app.get("/refresh_token", async (req, res) => {
+  try {
+    const refreshToken = await getRefreshToken();
+    const getNewAccess = await getNewAccessToken({
+      refresh_token: refreshToken.data.refresh_token,
+    });
+    const { access_token, expires_in } = getNewAccess.data;
+    let timer = Date.now();
+    let expiryTime = timer + expires_in;
+    const postNewAccess = await postNewAccessToken({
+      access_token: access_token,
+      expiredTime: expiryTime,
+    });
+    res.send(postNewAccess.data);
+  } catch (error) {
+    res.send(error.response);
+  }
 });
 
 app.listen(process.env.PORT || 8000, () => {
